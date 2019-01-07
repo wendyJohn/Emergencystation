f8f9eaed9a9bc968241db2728381579f3b17f430
qiaoshi <530913978@qq.com>
2019-01-07 16:14:01 +0800

    添加登录功能

---
+++ app/src/main/java/com/sanleng/emergencystation/activity/LoginActivity.java
@@ -0,0 +1,171 @@
+package com.sanleng.emergencystation.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.loopj.android.http.RequestParams;
+import com.sanleng.emergencystation.R;
+import com.sanleng.emergencystation.net.NetCallBack;
+import com.sanleng.emergencystation.net.RequestUtils;
+import com.sanleng.emergencystation.net.URLs;
+import com.sanleng.emergencystation.utils.PreferenceUtils;
+import com.sanleng.emergencystation.utils.StringUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+/**
+ * 登陆界面
+ *
+ * @author qiaoshi
+ */
+public class LoginActivity extends AppCompatActivity implements OnClickListener{
+    private EditText login_number;
+    private EditText login_password;
+    private Button login_btn;
+    private RelativeLayout scrollviewRootLayout;
+    private TextView login_questions;
+    private String userName;
+    private String password;
+    private String lastAccount;
+    private String lastPwd;
+    private CheckBox whether_contact;
+
+    @Override
+    protected void onCreate(Bundle arg0) {
+        super.onCreate(arg0);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+        setContentView(R.layout.login_activity);
+        initView();
+    }
+
+    private void initView() {
+        login_btn = (Button) findViewById(R.id.login_btn);
+        login_number = (EditText) findViewById(R.id.login_number);
+        login_password = (EditText) findViewById(R.id.login_password);
+        login_questions = (TextView) findViewById(R.id.login_questions);
+        login_btn.setOnClickListener(this);
+        login_password.setOnClickListener(this);
+
+        whether_contact = (CheckBox) findViewById(R.id.whether_contact);
+        whether_contact.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // TODO Auto-generated method stub
+                if (isChecked) {
+                    PreferenceUtils.setInt(LoginActivity.this, "EmergencyStation_state", 1);
+                } else {
+                    PreferenceUtils.setInt(LoginActivity.this, "EmergencyStation_state", 0);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        int state = PreferenceUtils.getInt(LoginActivity.this, "EmergencyStation_state");
+        if (state == 1) {
+            // 记住上次登录的信息
+            lastAccount = PreferenceUtils.getString(this, "EmergencyStation_username");
+            lastPwd = PreferenceUtils.getString(this, "EmergencyStation_password");
+            if (!StringUtils.isEmpty(lastAccount) && !StringUtils.isEmpty(lastPwd)) {
+                login_number.setText(lastAccount);
+                login_password.setText(lastPwd);
+                Intent intent_pwdchange = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent_pwdchange);
+                finish();
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login_btn:
+                userName = login_number.getText().toString().trim();
+                password = login_password.getText().toString().trim();
+                RequestParams params = new RequestParams();
+                params.put("username", userName);
+                params.put("password", password);
+                params.put("platformkey", "app_firecontrol_owner");
+                RequestUtils.ClientPost(URLs.BULOGIN_URL, params, new NetCallBack() {
+                    @Override
+                    public void onStart() {
+
+                        super.onStart();
+                    }
+
+                    @Override
+                    public void onMySuccess(String result) {
+                        if (result == null || result.length() == 0) {
+                            return;
+                        }
+
+                        System.out.println("数据请求成功" + result);
+                        try {
+                            JSONObject jsonObject = new JSONObject(result);
+                            String msg = jsonObject.getString("msg");
+
+                            if (msg.equals("登录成功")) {
+
+
+                                String data = jsonObject.getString("data");
+                                JSONObject object = new JSONObject(data);
+                                String unitcode = object.getString("unitcode");
+                                String agentName = object.getString("name");
+
+                                //绑定唯一标识
+//                                JPushInterface.setAlias(LoginActivity.this, 1, unitcode);
+
+                                // 存入数据库中（登录名称和密码）
+                                PreferenceUtils.setString(LoginActivity.this, "EmergencyStation_username", userName);
+                                PreferenceUtils.setString(LoginActivity.this, "EmergencyStation_password", password);
+                                // 单位ID
+                                PreferenceUtils.setString(LoginActivity.this, "unitcode", unitcode);
+                                // 人员名称
+                                PreferenceUtils.setString(LoginActivity.this, "agentName", agentName);
+
+                                new Handler().postDelayed(new Runnable() {
+                                    public void run() {
+                                        // 等待2000毫秒后销毁此页面，并提示登陆成功
+                                        Intent intent_pwdchange = new Intent(LoginActivity.this, MainActivity.class);
+                                        startActivity(intent_pwdchange);
+                                        finish();
+                                    }
+                                }, 1000);
+                            } else {
+
+                            }
+
+                        } catch (JSONException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onMyFailure(Throwable arg0) {
+
+
+                    }
+                });
+
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/sanleng/emergencystation/activity/WelcomeActivity.java
@@ -0,0 +1,31 @@
+package com.sanleng.emergencystation.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.sanleng.emergencystation.R;
+
+/**
+ * 引导界面
+ *
+ * @author Qiaoshi
+ * @date 创建时间：2019年01月07日
+ */
+
+public class WelcomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.welcome_activity);
+                new Handler().postDelayed(new Runnable() {
+                    public void run() {
+                        Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }, 1000);
+
+    }
+}
